deb333bbef3c7b026973636f9a71f444d0dd7997
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-02-19 09:27:51 -0500

    Set state to the editing form

--- client/src/Movies/UpdateMovieForm.js
+++ client/src/Movies/UpdateMovieForm.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState, UseEffect } from "react";
 
 const UpdateMovieForm = () => {
+  const [inputs, setInputs] = useState({
+    id: null,
+    title: '',
+    director: '',
+    metascore: '',
+    stars: []
+  });
 
   return (
     <div className="update__form__container">
       <form className="update__form">
-        <input type="text" name="title" />
-        <input type="text" name="director" />
-        <input type="text" name="metascore" />
-        <input type="text" name="actors" />
+        <label htmlFor="title">Movie Title</label>
+        <input type="text" name="title" id="title" value={inputs.title} />
+
+        <label htmlFor="director">Director</label>
+        <input type="text" name="director" id="director" value={inputs.director} />
+
+        <label htmlFor="metascore">Metascore</label>
+        <input type="text" name="metascore" id="metascore" value={inputs.metascore} />
+
+        <h2>Actors</h2>
+        {inputs.stars.map((star, idx) => (
+          <input key={idx} type="text" name="stars" value={inputs.stars[idx]} />
+        ))}
+
         <button>Cancel</button>
         <button>Update</button>
       </form>
