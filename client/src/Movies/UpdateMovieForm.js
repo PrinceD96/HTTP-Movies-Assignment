a97a5d531cce5e481a695f5738a4a3ce94f63c2b
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-02-19 10:12:09 -0500

    Edit form inputs working properly. Working on the PUT request

--- client/src/Movies/UpdateMovieForm.js
+++ client/src/Movies/UpdateMovieForm.js
@@ -17,21 +17,36 @@ const UpdateMovieForm = props => {
       .catch(error => console.error(error))
   }, [props.match.params.id])
 
+  const handleChange = e => {
+    if (e.target.name !== "stars") {
+      setInputs({ ...inputs, [e.target.name]: e.target.value })
+    } else {
+      const id = e.target.getAttribute('actor-id');
+      const newStars = [...inputs.stars];
+      newStars[id] = e.target.value;
+      setInputs({
+        ...inputs,
+        stars: newStars
+      });
+    }
+    console.log(e.target.name, inputs[e.target.name])
+  }
+
   return (
     <div className="update__form__container">
       <form className="update__form">
         <label htmlFor="title">Movie Title</label>
-        <input type="text" name="title" id="title" value={inputs.title} />
+        <input type="text" name="title" id="title" value={inputs.title} onChange={handleChange} />
 
         <label htmlFor="director">Director</label>
-        <input type="text" name="director" id="director" value={inputs.director} />
+        <input type="text" name="director" id="director" value={inputs.director} onChange={handleChange} />
 
         <label htmlFor="metascore">Metascore</label>
-        <input type="text" name="metascore" id="metascore" value={inputs.metascore} />
+        <input type="text" name="metascore" id="metascore" value={inputs.metascore} onChange={handleChange} />
 
         <h2>Actors</h2>
-        {inputs.stars.map((star, idx) => (
-          <input key={idx} type="text" name="stars" value={inputs.stars[idx]} />
+        {inputs.stars.map((star, index) => (
+          <input key={index} actor-id={index} type="text" name="stars" value={inputs.stars[index]} onChange={handleChange} />
         ))}
 
         <button>Cancel</button>
